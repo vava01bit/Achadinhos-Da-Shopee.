02cb93288074ecbf2941a96ddec9e7492537b688
vava01bit <marhyvicmv@gmail.com>
2025-02-14 09:56:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error("Erro ao carregar produtos:", error));
 });
+function filtrarProdutos() {
+    const termoBusca = document.getElementById("searchBar").value.toLowerCase();
+    const produtos = document.querySelectorAll(".produto");
+
+    produtos.forEach(produto => {
+        const nomeProduto = produto.querySelector("h3").innerText.toLowerCase();
+        if (nomeProduto.includes(termoBusca)) {
+            produto.style.display = "block";
+        } else {
+            produto.style.display = "none";
+        }
+    });
+}
